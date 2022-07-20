f8dafbfe919c32265d5db45d9077bc43fcbe5e13
RedBoard <ott.thomas68@gmail.com>
2022-07-20 19:53:15 +0200

    (|) modify pool api

--- check_block.py
+++ check_block.py
@@ -1,3 +1,4 @@
+from time import time
 import requests
 from discord.ext import tasks
 from get_data import f_get_height, f_set_height
@@ -8,20 +9,18 @@ def request_json(url:str):
     except requests.exceptions.ConnectionError:
         return (None)
 
-async def send_notification_block(bot, block_l):
+async def send_notification_block(bot):
     role_id = "<@419926802366988292>"
     channel = bot.get_channel(933874035966754946)
-    await channel.send(role_id + " | New blocks for solo ERGO !!!")
+    await channel.send(role_id + " | New blocks for solo mining !!!")
 
 @tasks.loop(seconds = 10)
 async def check_new_block(bot):
-    rsp = request_json("https://solo-erg.2miners.com/api/blocks")
+    rsp = request_json("https://minenice.newpool.pw:8201/stats_address?address=GSw7npbaVSrjNCYxuRnEyz8My1cn696HaS&longpoll=false")
     if (rsp == None):
         return (None)
-    candidates:list = rsp['candidates']
-    for block_l in candidates:
-        height:int = int(block_l['height'])
-        block_adress:str = block_l['finder']
-        if (height > f_get_height() and block_adress == '9fgtyaC2oMUssJCWRS1AcPtSZsxcx1MmvhmySk2QNxA3i3KvCio'):
-            f_set_height(height)
-            await send_notification_block(bot, block_l)
+    last_payments = rsp['payments'][0]
+    l_last_payments = last_payments.split(':')
+    if (l_last_payments[4] != f_get_height()):
+        f_set_height(l_last_payments[4])
+        await send_notification_block(bot)
